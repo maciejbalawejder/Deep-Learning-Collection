7d415023ce6cbf3c0b1da40e5fd9fcb790832a28
Maciej Balawejder <maciekstar6@gmail.com>
2021-10-08 23:21:35 +0100

    fix update

--- ConvNets/U-Net/data/dataset_generator.py
+++ ConvNets/U-Net/data/dataset_generator.py
@@ -13,10 +13,10 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class Cityscapes(Dataset):
-  def __init__(self, root_dir, transform = None):
-    self.root_dir = root_dir
-    self.images = os.listdir(self.root_dir+'inputs/')
-    self.labels = os.listdir(self.root_dir+'labels/')
+  def __init__(self, path, transform = None):
+    self.root_dir = path
+    self.images = self.root_dir+'inputs/'
+    self.labels = self.root_dir+'labels/'
     assert len(self.labels) == len(self.images)
     self.transform = transform
   
@@ -24,8 +24,8 @@ class Cityscapes(Dataset):
     return len(self.labels)
   
   def __getitem__(self, index):
-    image = plt.imread(root_dir + 'inputs/' + str(index+1) + '.jpg') / 255
-    label = plt.imread(root_dir + 'labels/' + str(index+1) + '.jpg') / 255
+    image = plt.imread(self.images + str(index+1) + '.jpg') / 255
+    label = plt.imread(self.labels + str(index+1) + '.jpg') / 255
 
     if self.transform is not None:
       augmentations = self.transform(image = image, mask = label)
