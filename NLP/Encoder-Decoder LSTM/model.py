8844bfd8a2f695f70a156be35040d72303e01c17
Maciej Balawejder <maciekstar6@gmail.com>
2021-11-21 22:30:11 +0100

    train/test/validation step

--- NLP/Encoder-Decoder LSTM/model.py
+++ NLP/Encoder-Decoder LSTM/model.py
@@ -11,17 +11,16 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torchtext.data.metrics import bleu_score
-from utils import i2w
 
 class Model:
     def __init__(self, model, lr, vocab, device):
         self.model = model
-        self.opt = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.opt = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.loss = nn.CrossEntropyLoss(ignore_index=vocab.vocab.stoi["<pad>"]) # ignore paddings while calculating loss
         self.vocab = vocab
         self.device = device
 
-    def train_step(self, dataset):
+    def train_step(self, dataset, force_ratio):
         epoch_loss = []
         bleu = []
         self.model.train()
@@ -29,8 +28,8 @@ class Model:
             src = batch.src.to(self.device)
             trg = batch.trg.to(self.device)
             self.opt.zero_grad()
-            outputs = self.model(src, trg).to(self.device)
-            loss = self.loss(outputs, trg)
+            outputs = self.model(src, trg, force_ratio=force_ratio).to(self.device)
+            loss = self.loss(outputs[1:].reshape(-1,outputs.shape[2]), trg[1:].reshape(-1))
             loss.backward()
 
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1) # clipping gradients
@@ -38,7 +37,7 @@ class Model:
             epoch_loss.append(loss.item())
 
             source = i2w(trg, self.vocab)
-            target = i2w(outputs.argmax(1), self.vocab, target=True)
+            target = i2w(outputs.argmax(2), self.vocab, target=True)
 
             bleu.append(bleu_score(source, target))
 
@@ -54,12 +53,12 @@ class Model:
             for batch in dataset:
                 src = batch.src.to(self.device)
                 trg = batch.trg.to(self.device)
-                outputs = self.model(src, trg, force_ratio=1).to(self.device)
-                loss = self.loss(outputs, trg)
+                outputs = self.model(src, trg, force_ratio=0).to(self.device)
+                loss = self.loss(outputs[1:].reshape(-1,outputs.shape[2]), trg[1:].reshape(-1))
                 epoch_loss.append(loss.item())
 
                 source = i2w(trg, self.vocab)
-                target = i2w(outputs.argmax(1), self.vocab, target=True)
+                target = i2w(outputs.argmax(2), self.vocab, target=True)
             
                 bleu.append(bleu_score(source, target))
 
@@ -73,10 +72,10 @@ class Model:
             for batch in dataset:
                 src = batch.src.to(self.device)
                 trg = batch.trg.to(self.device)
-                outputs = self.model(src, trg, force_ratio=1).to(self.device)
+                outputs = self.model(src, trg, force_ratio=0).to(self.device)
 
                 source = i2w(trg, self.vocab)
-                target = i2w(outputs.argmax(1), self.vocab, target=True)
+                target = i2w(outputs.argmax(2), self.vocab, target=True)
                 score = bleu_score(source, target)
                 bleu.append(score)
         return np.mean(bleu) * 100
