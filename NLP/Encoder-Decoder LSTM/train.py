0df67b716c3b09050d99df33de20e267f86e720c
Maciej Balawejder <maciekstar6@gmail.com>
2021-11-30 16:26:05 +0000

    training loop fix

--- NLP/Encoder-Decoder LSTM/train.py
+++ NLP/Encoder-Decoder LSTM/train.py
@@ -17,57 +17,59 @@ import matplotlib.pyplot as plt
 #from google.colab import drive => uncomment for colab
 #drive.mount('/content/drive')
 
-BATCH = 128
+BATCH = 64
 LEARNING_RATE = 0.001
-LAYERS = 2
-HIDDEN_DIM = 256
-EPOCHS = 25
+LAYERS = 1
+HIDDEN_DIM = 1024
+EPOCHS = 20
 EMBEDDING_DIM = 300
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-P = 0.5 
+P = 0.5
 
 # DATASET
-(train_iterator, validation_iterator, test_iterator), (train_data, validation_data, test_data), (english, german) = datasetGenerator(BATCH, DEVICE, 30*128)
+(train_iterator, validation_iterator, test_iterator), (train_data, validation_data, test_data), (english, german) = datasetGenerator(BATCH, DEVICE)
 
 # MODEL
 ENCODER = Encoder(input_size=len(german.vocab), embedding_size=EMBEDDING_DIM, 
-                  hidden_size=HIDDEN_DIM, layers=LAYERS, p=P, bidirectional=False).to(DEVICE)
+                  hidden_size=HIDDEN_DIM, layers=LAYERS, p=P).to(DEVICE)
 
 DECODER = Decoder(input_size=len(english.vocab), output_size=len(english.vocab), 
                   embedding_size=EMBEDDING_DIM, hidden_size=HIDDEN_DIM,
-                  layers=LAYERS, p=P, bidirectional=False).to(DEVICE)
+                  layers=LAYERS, p=0).to(DEVICE)
 
 SEQ2SEQ = Seq2Seq(ENCODER, DECODER).to(DEVICE)
 MODEL = Model(model=SEQ2SEQ, lr=LEARNING_RATE, vocab=english, device=DEVICE)
-STEPLR = torch.optim.lr_scheduler.StepLR(MODEL.opt, step_size=10, gamma=0.5)
+lambda2 = lambda epoch: round(0.7 ** epoch,7)
+STEPLR = torch.optim.lr_scheduler.LambdaLR(MODEL.opt, lr_lambda=lambda2)
 
 # TRAINING LOOP
 force_ratio = 0.5
-train_loss, train_bleu = [], [0]
-val_loss, val_bleu = [], [0] # added zero at the start to avoid error with max
+train_loss, train_bleu = [], []
+val_loss, val_bleu = [0], [] # added zero at the start to avoid error with max
 lr = []
-for epoch in tqdm_notebook(range(1, 25+1), desc = 'Epoch'):
-    tl, tb = MODEL.train_step(train_iterator, force_ratio)
+for epoch in tqdm_notebook(range(1, EPOCHS+1), desc = 'Epoch'):
+    tl, _ = MODEL.train_step(train_iterator, force_ratio)
     vl, vb = MODEL.validation_step(validation_iterator)
 
-    if vb > max(val_bleu):
-        save_checkpoint(MODEL.checkpoint, 'drive/MyDrive/model128')
-    
-    if epoch%5 == 0 and force_ratio > 1e-6:
-        force_ratio = force_ratio * 0.5
+    if  epoch > 2 and (vl-tl) > 0.1:
+        print("Overfitting...")
+        break
 
-    STEPLR.step()
+    if vl < min(val_loss):
+        save_checkpoint(MODEL.checkpoint, 'drive/MyDrive/attentionv1')
+    if epoch&2 == 0 : STEPLR.step()
+    force_ratio = round(force_ratio*0.5,5)
+        
 
     train_loss.append(tl)
-    train_bleu.append(tb)
     val_loss.append(vl)
     val_bleu.append(vb)
     lr.append(STEPLR.get_lr())
     
+    print(f'Delta loss: {vl-tl}')
+    print(f'Bleu score: {vb}')
     
-    #print(f'Bleu score: {tb}, {vb}')
-    #print(f'Learning rate: {lr[-1]} Force ratio : {force_ratio}')
-
+    
 MODEL.test_step(test_iterator)
 
 plt.figure(dpi=200)
