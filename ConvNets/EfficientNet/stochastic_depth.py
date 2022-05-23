876297e710ddafa7039d5a16e77db5903eab5eb7
Maciej Balawejder <maciekstar6@gmail.com>
2022-05-23 16:59:27 +0200

    upload stochastic_depth.py

---
+++ ConvNets/EfficientNet/stochastic_depth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import torch
+from torch import Tensor
+import torch.nn as nn 
+
+
+# In[49]:
+
+
+class StochasticDepth(nn.Module):
+    " Stochastic Depth / Drop Path module which takes survival probability(p) and returns input with *dropped* examples in the batch. "
+    
+    def __init__(
+        self,
+        p : float = 0.5
+        ):
+        super().__init__()
+        self.p = p
+        
+    def forward(self, x: Tensor) -> Tensor:
+        mask_shape = (x.shape[0],) + (1,)*(x.ndim - 1)
+        # mask shape: [batch, 1, 1, 1]
+        mask = torch.empty(mask_shape).bernoulli_(self.p) / self.p
+        
+        if self.training : x = mask * x
+        return x 
+
