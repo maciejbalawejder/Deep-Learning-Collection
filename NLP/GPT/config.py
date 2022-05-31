c58ae909f2b3df28d97d907b52173522b8088d16
Maciej Balawejder <maciekstar6@gmail.com>
2022-05-31 08:53:32 +0100

    Update config.py

--- NLP/GPT/config.py
+++ NLP/GPT/config.py
@@ -1,21 +1,9 @@
 class Config:
-    def __init__(
-        self,
-        vocab_size : int = 5_000,
-        window : int = 512,
-        d_model = 768,
-        layers : int = 12,
-        p : float = 0.1,
-        heads : int = 12,
-        inner_state : int = 3072,
-        device : str = "cpu"
-        ):
-
-        self.vocab_size = vocab_size
-        self.window = window
-        self.layers = layers
-        self.p = p
-        self.heads = heads
-        self.inner_state = inner_state
-        self.d_model = d_model
-        self.device = device
+    vocab_size : int = 5_000,
+    window : int = 512,
+    d_model = 768,
+    layers : int = 12,
+    p : float = 0.1,
+    heads : int = 12,
+    inner_state : int = 3072,
+    device : str = "cpu"
